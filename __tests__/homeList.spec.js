6c39de7c1560b536456d0324a789831ba9feb088
Jing Zhao <zhaojing19890531@163.com>
2017-03-20 17:27:58 +0800

    添加截屏测试

---
+++ __tests__/homeList.spec.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import homeLike from '../app/components/Home/homeList'
+
+it('renders correctly', () => {
+  // const tree = renderer.create(
+  //   <Index />
+  // );
+});
